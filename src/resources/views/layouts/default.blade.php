9d949125900a0f6ab81e2a67384444dfec589e02
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-08-27 18:33:06 +0900

    初期ローダーのoverlay方法を修正

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -2,13 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @yield('head')
 <body>
-<div id="pre-loader" class="position-fixed bg-white" style="width: 100%; height: 100%; top: 0; opacity: 0.8;">
-    <div class="d-flex justify-content-center" style="margin-top: 40%;">
-        <div class="spinner-grow text-secondary" role="status" style="width: 3rem; height: 3rem;">
-            <span class="sr-only">Loading...</span>
-        </div>
+<div id="pre-loader" class="d-flex align-items-center justify-content-center overlay">
+    <div class="spinner-grow text-secondary" role="status" style="width: 3rem; height: 3rem;">
+        <span class="sr-only">Loading...</span>
     </div>
 </div>
+<style>
+    .overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        z-index: 9999;
+        background: rgba(255, 255, 255, .8);
+    }
+</style>
 <div id="app">
     @yield('content')
 </div>
