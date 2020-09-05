71e0b55561446ac728f3837f2a7c6ab1d5b644a9
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-09-06 02:15:01 +0900

    テキストフォームコンポーネントを追加

---
+++ src/resources/js/atoms/Inputs/InputText.vue
@@ -0,0 +1,48 @@
+<template>
+  <input
+    v-model="text"
+    type="text"
+    class="form-control"
+    :placeholder="placeholder"
+    :max="max"
+    :min="min"
+  >
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    max: {
+      type: Number,
+      default: 100
+    },
+    min: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    text: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
